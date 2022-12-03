48f16a2f78eae2ff3f2781f0e981d2b238728728
shubham kumar <57143760+singh235kumar@users.noreply.github.com>
2022-12-03 22:07:31 +0530

    Update SearchScreen.js

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,44 +1,45 @@
-import  { useState } from "react"
-import { View, Text, StyleSheet, FlatList } from "react-native";
+import react, {useState} from "react"
+import {View, Text, StyleSheet, FlatList} from "react-native";
 import Search from "../components/Search";
 import axios from "axios";
 import Article from "../components/Article";
 
 const SearchScreen = () => {
-    const [searchText, setSearchText] = useState("");
-    const [articles, setArticles] = useState([]);
+    const [searchText,setSearchText] = useState("");
+    const [articles,setArticles] = useState([]);
 
-    const searchArticles = () => {
-        axios.get('https://newsapi.org/v2/top-headlines?country=in&apiKey=e225a060a7944d599389c0cb84986243',{
-            params:{
-                category: "sports",
-            }
-        })
-            .then((response) => {
-                // handle success
-                setArticles(response.data.articles);
-            })
-            .catch(function (error) {
-                // handle error
-                console.log(error);
-            })
-            .then(function () {
-                // always executed
-            });
-    }
+   const searchArticles = () =>{
+       axios.get('https://newsapi.org/v2/top-headlines?country=in&apiKey=e225a060a7944d599389c0cb84986243',{
+           params:{
+               //category: "technology",
+               q: searchText
+           }
+       })
+           .then( (response) =>{
+               // handle success
+               setArticles(response.data.articles);
+           })
+           .catch(function (error) {
+               // handle error
+               console.log(error);
+           })
+           .then(function () {
+               // always executed
+           });
+   }
     return (
         <View style={styles.container}>
-            <Search searchText={searchText} setSearchText={setSearchText} onSubmit={searchArticles} />
+            <Search searchText={searchText} setSearchText={setSearchText} onSubmit={searchArticles}/>
             <FlatList
                 data={articles}
-                renderItem={({ item }) =>
+                renderItem = {({item}) =>
                     <Article
-                        urlToImage={item.urlToImage}
-                        title={item.title}
-                        description={item.description}
-                        author={item.author}
-                        publishedAt={item.publishedAt}
-                        sourceName={item.source.name}
+                        urlToImage = {item.urlToImage}
+                        title = {item.title}
+                        description = {item.description}
+                        author = {item.author}
+                        publishedAt = {item.publishedAt}
+                        sourceName = {item.source.name}
                     />}
                 keyExtractor={(item) => item.title}
             />
